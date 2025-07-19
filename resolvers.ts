f02dea91a3cab9e60569db932afbd831822e3e74
vuvantoann <toanvu1728@gamil.com>
2025-07-19 11:50:18 +0700

    Lấy ra danh sách bài viết

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,16 @@
+import Article from './models/task.model'
+
 export const resolvers = {
   Query: {
     hello: () => {
       return 'Toàn đẹp trai'
+    },
+    getListArticle: async () => {
+      const articles = await Article.find({
+        deleted: false,
+      })
+
+      return articles
     },
   },
 }
--- typeDefs.ts
+++ typeDefs.ts
@@ -1,6 +1,14 @@
 import { gql } from 'apollo-server-express'
 export const typeDefs = gql`
+  type Article {
+    id: ID
+    title: String
+    avatar: String
+    description: String
+  }
+
   type Query {
     hello: String
+    getListArticle: [Article]
   }
 `
