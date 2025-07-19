b2b9414e8bd175ce9ea75d6668d838d1cd0e7d21
vuvantoann <toanvu1728@gamil.com>
2025-07-19 11:31:35 +0700

    Cấu hình với graphql cơ bản

--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
-console.log('Oke')
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import * as database from './config/database'
+import { ApolloServer } from 'apollo-server-express'
+import { typeDefs } from './typeDefs'
+import { resolvers } from './resolvers'
+
+const startServer = async () => {
+  dotenv.config()
+  database.connect()
+  const app: Express = express()
+  const port: string | number = process.env.PORT || 3000
+
+  //graphql
+
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  })
+
+  await apolloServer.start()
+
+  apolloServer.applyMiddleware({
+    app: app,
+    path: '/graphql',
+  })
+  app.listen(port, () => {
+    console.log('lắng nghe thành công ')
+  })
+}
+
+startServer()
---
+++ models/task.model.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const articleSchema = new mongoose.Schema(
+  {
+    title: String,
+    avatar: String,
+    description: String,
+    categoryId: String,
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Article = mongoose.model('Article', articleSchema, 'articles')
+
+export default Article
